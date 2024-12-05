d4a09e78025a1c20dacf6a3d449d1e000a4a8a1f
Senan Efendi <Senan-Efendiev>
2024-12-06 00:48:40 +0500

    Migration task

--- database/migrations/2024_12_05_114726_create_reviews_table.php
+++ database/migrations/2024_12_05_114726_create_reviews_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('game_id')->on('games')->constrained()->onDelete('cascade');
+            $table->foreignId('game_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->integer('rating');
             $table->text('comment')->nullable();
